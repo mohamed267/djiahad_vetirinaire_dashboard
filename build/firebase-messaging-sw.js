55f5799674628250c762a51ebc97ef9eb48b23ad
mohamed267 <m.rabahsidhoum@esi-sba.dz>
2022-08-05 10:53:05 -0700

    fix deploy essue

--- build/firebase-messaging-sw.js
+++
@@ -1,6 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-firebase.initializeApp({
-    messagingSenderId: "483656068603",
-});
-const messaging = firebase.messaging();
